358f29e2673573457f0331db86d52f5002b2d2a4
Giordano Meza Rivas <giordano@axcs.se>
2025-06-02 20:52:12 +0200

    Add project files.

---
+++ Travelize_CodeTest/OneHourTimedAttempt.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travelize_CodeTest
+{
+    public class OneHourTimedAttempt
+    {
+        public void Solution()
+        {
+            Console.WriteLine("=== Club Member Registration ===");
+
+            Console.WriteLine("Enter name");
+            var Name = Console.ReadLine();
+
+            ValidateName(Name);
+
+            /////Note: slänga exception istället för konsol meddelande??
+            //if (Name.Length < 5)
+            //{
+            //    Console.WriteLine("Name should be 5 characters or more");
+            //}
+
+
+
+            Console.WriteLine("Enter Birthdate in format yyyyMMdd");
+            var Birthdate = Console.ReadLine();
+            DateTime? today = DateTime.Today;
+            DateTime? parsedDate = null;
+
+
+            ///No bueno, för mycket i try blocket, behöver brytas ut för att fånga olika saker.??
+            try
+            {
+                parsedDate = DateTime.ParseExact(Birthdate, "yyyyMMdd", CultureInfo.InvariantCulture);
+                TimeSpan ts = (TimeSpan)(today - parsedDate);
+
+
+
+                if (true)
+                {
+                    var userData = "placeholder\t";
+                    /// adduserdatamethod();
+                    /// 
+
+
+                    ///fix strucuter below
+                    var path = @"C:\Users\Furball\Desktop\Desktop";
+
+                    if (!File.Exists(path))
+                    {
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(userData);
+                            sw.Close();
+                        }
+                    }
+
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.Write(userData);
+                        sw.Close();
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid Birtdate format");
+                ///Note, slänag tillbaka till början?
+            }
+
+            static void ValidateName(string name)
+            {
+                if (name.Length < 5)
+                    throw new ArgumentException("Name must be at least 5 characters long.");
+            }
+
+            static void ValidateAge(DateTime birthdate)
+            {
+                int currentYear = DateTime.Now.Year;
+                int birthYear = birthdate.Year;
+
+                if (currentYear - birthYear < 15)
+                    throw new ArgumentException("Member must be at least 15 years old.");
+            }
+
+            static void SaveMembers()
+            {
+
+            }
+        }
+    }
+}
---
+++ Travelize_CodeTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Member Registration");
+	while (true)
+	{
+		Console.WriteLine("Enter Name or press ENTER to quit.");
+		var nameInput = Console.ReadLine();
+
+		if (string.IsNullOrEmpty(nameInput))
+		{
+	        Console.WriteLine("Exiting member registration...");
+		    break;
+		}
+
+		try
+		{
+			ValidateNameInput(nameInput);
+
+			Console.WriteLine("Enter birthdate (yyyy-MM-dd)");
+			var birthdateInput = Console.ReadLine();
+
+			var isCorrect = DateTime.TryParseExact(birthdateInput, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime birthdate);
+			if (!isCorrect)
+			{
+				throw new FormatException("Invalid birthdate format. Please use yyyy-MM-dd.");
+			}
+
+			ValidateAgeIsCorrect(birthdate);
+			SaveRegistratedMembers(nameInput, birthdate);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine($"And error has been thrown: {e.Message}\n");
+		}
+	}
+Console.WriteLine("Added members have been saved to file. Shutting down...");
+
+static void ValidateNameInput(string? nameInput)
+{
+	if (nameInput.Length < 5)
+		throw new ArgumentException("Name must be at least 5 characters long.");
+}
+static void ValidateAgeIsCorrect(DateTime birthdate)
+{
+	int currentYear = DateTime.Now.Year;
+	int birthYear = birthdate.Year;
+	int memberYear = currentYear - birthYear;
+	if (memberYear < 15)
+		throw new ArgumentException($"The user you are trying to add is {memberYear} years old. Members must be at least 15 years old.");
+}
+static void SaveRegistratedMembers(string name, DateTime birthdate)
+{
+	///Will be saved int he bin folder
+	var filePath = "members.txt";
+	
+	using (StreamWriter sw = new StreamWriter(filePath, append: true))
+    {
+        sw.WriteLine($"{name}\t{birthdate:yyyy-MM-dd}");
+        Console.WriteLine("Member has been saved!");
+	}
+}
